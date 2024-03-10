88ad8747fef35c5249908cdb339c0d1cf0161d79
Rafael <rafael.constantino89@gmail.com>
2024-03-10 00:53:33 -0300

    backend-feat: add express initial server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hellow");
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/health", (_req: Request, res: Response) => res.send("I am alive!"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port} 🚀`);
+});
+
+export default app;
